5ce8102800ab116aa11639c87883f27c7cec73f6
janghyun <janghyun0401@naver.com>
2025-02-20 17:06:36 +0900

    App.js 수정

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,6 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
-// import Header from "./Header";
 import GroupInfo from "./GroupInfo";
-import MemoryList from "./MemoryList";
 import PostDetail from "./PostDetail";
 import MemoryCreatePage from "../pages/CreateGroupPage";
 
@@ -10,7 +8,6 @@ const App = () => {
   return (
     <Routes>
       <Route path="/group/info" element={<GroupInfo />} />
-      <Route path="/group/memories" element={<MemoryList />} />
       <Route path="/groups/posts/create" element={<MemoryCreatePage />} />
       {/*"/groups/:groupId/posts/create"*/}
       <Route path="/groups/posts/:id" element={<PostDetail />} />
